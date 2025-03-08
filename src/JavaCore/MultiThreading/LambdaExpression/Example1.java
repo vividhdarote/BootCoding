23b3b2b1cf824a63e5879d4a92e7f18e6e114930
VIVIDHDAROTE <darotevividh@gmail>
2025-03-09 00:57:52 +0530

    Adding MultiThreading: Lambda Expression and Thread Communication

---
+++ src/JavaCore/MultiThreading/LambdaExpression/Example1.java
@@ -0,0 +1,70 @@
+package JavaCore.MultiThreading.LambdaExpression;
+
+public class Example1 {
+    public static void main(String[] args) throws InterruptedException {
+
+        //1.
+        // Runnable is an interface class with default abstract method -> public void run()
+        //Anonymous interface class
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.print("Hello ");
+            }
+        };
+        Thread thread = new Thread(runnable);
+        thread.start();
+        Thread.sleep(1000);
+
+
+        //2.
+        // Using Lambda Expression
+        // this avoids to write access modifier, variable, return type
+        Runnable runnable1 = () -> {
+            System.out.print("World ");
+        };
+        Thread thread1 = new Thread(runnable1);
+        thread1.start();
+        Thread.sleep(1000);
+
+
+        //3.
+        //Another Lambda Expression example for non-return type -> void()
+        // and avoid Curly Braces
+        Runnable runnable2 = () -> System.out.print("Its Vividh ");
+        Thread thread2 = new Thread(runnable2);
+        thread2.start();
+        Thread.sleep(1000);
+
+
+        //4.
+        // Directly used method in Thread
+        Thread thread3 = new Thread(()-> System.out.println("Here"));
+        thread3.start();
+
+
+        //5.
+        Runnable runnable3 = ()->{
+            for(int i=0;i<2;i++){
+                System.out.println("Its easy");
+            }
+        };
+        Thread thread5 = new Thread(runnable3);
+        thread5.start();
+
+
+        //6.
+        Thread thread4 = new Thread(()->{
+            for(int i=0;i<5;i++){
+                System.out.println("I Love Java");
+            }
+        });
+        thread4.start();
+
+
+
+    }
+}
+
+
+
---
+++ src/JavaCore/MultiThreading/LambdaExpression/Example2.java
@@ -0,0 +1,78 @@
+package JavaCore.MultiThreading.LambdaExpression;
+
+
+interface Student{
+    public String setName(String name);
+}
+
+interface Student1{
+    public void setName(String name);
+}
+
+
+
+class College implements Student{
+
+    @Override
+    public String setName(String name) {
+        return name+" call from implementation class";
+    }
+}
+
+
+
+public class Example2 {
+    public static void main(String[] args) {
+
+        //1.
+        //Common method
+        College college = new College();
+        String string = college.setName("Aman");
+        System.out.println(string);
+
+
+        //2.
+        //Anonymous interface class
+        //This avoid the use of College implementation class
+        Student engineeringStudent= new Student() {
+            @Override
+            public String setName(String name) {
+                return  name+" is a Engineering Student.";
+            }
+        };
+        String name1 = engineeringStudent.setName("Anshul");
+        System.out.println(name1);
+
+
+
+        //3.
+        //Using Lambda Expression interface class
+        // you can also avoid writing datatype of interface in parentheses
+        // (String name) can replace by (name) only
+        Student mcaStudent = (name) ->{           //(String name) -> (name)
+          return  name+" is a MCA Student.";
+        };
+        String name2 = mcaStudent.setName("Vividh");
+        System.out.println(name2);
+
+
+
+        //4.
+        //Another Lambda Expression example
+        //without using return keyword and avoid using curly braces
+        Student mcaStudent1 = (name)-> name+" is a MCA Student";
+        String name3= mcaStudent1.setName("Prasanna");
+        System.out.println(name3);
+
+
+
+        //5.
+        //Another Lambda Expression example for non-return type -> void()
+        // Allow Only for single argument/parameter
+        // and avoid Curly Braces
+        Student1 voidStudent = name -> System.out.println(name+ " is call from non-return type");
+        voidStudent.setName("Donald");
+
+
+    }
+}
