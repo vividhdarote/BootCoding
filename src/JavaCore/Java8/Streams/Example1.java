e8e03095776bb351194534b4e33f072a0528260b
VIVIDHDAROTE <darotevividh@gmail>
2025-03-12 00:30:36 +0530

    Adding Java 8: Stream

--- src/JavaCore/Java8/Streams/Example1.java
+++ src/JavaCore/Java8/Streams/Example1.java
@@ -8,10 +8,10 @@ public class Example1 {
 
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         int sum = list.stream().filter(x -> x % 2 == 0).mapToInt(x -> x).sum();
- //                                    -----------------------
- //                                               ^
- //                                               |
- //                                           Predicate
+ //                                    ----------------
+ //                                            ^
+ //                                            |
+ //                                        Predicate
 
 
         System.out.println(sum);
