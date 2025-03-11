2e7f7cf1db8693ac4752a1ba8f2ee1c1531979aa
VIVIDHDAROTE <darotevividh@gmail>
2025-03-12 03:03:40 +0530

    Adding Java 8: Function Interface

---
+++ src/JavaCore/Java8/Function_Interfaces/Supplier/Example1.java
@@ -0,0 +1,30 @@
+package JavaCore.Java8.Function_Interfaces.Supplier;
+
+//This is a functional interface whose functional method is get().
+// Single Abstract method
+
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class Example1 {
+    public static void main(String[] args) {
+
+        Supplier<Integer> supp = () -> 1;
+        System.out.println(supp.get());
+
+
+        Predicate<Integer> predicate = x -> x % 2 == 0;
+        Function<Integer, Integer> function = x -> x * x;
+        Consumer<Integer> consumer = x -> System.out.println(x);
+        Supplier<Integer> supplier = () -> 100;
+
+        if (predicate.test(supplier.get())) {
+            consumer.accept(function.apply(supplier.get()));
+        }
+
+
+    }
+}
